20e2d3596da1fe8fd7224ff0eae34cc1d44b2d6a
Anthony Morales <56373049+Antwon9@users.noreply.github.com>
2025-01-15 01:02:47 +0000

    FinalCommit

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,17 +1,65 @@
 const Node = require('./Node.js');
 
 class LinkedList {
+
 	constructor() {
 		this.head = null;
+		this.size = 0;
 	}
 
 	print(cb = console.log) {
+		let current = this.head;
+		if (current == null){
+
+		}else{
+			let i = 0;
+			while (i < this.size){
+				cb(current.value);
+				if(current.next == null){
+
+				}else{
+					current = current.next;
+				}
+				i++;
+			}
+		}
+
 	}
 
 	append(value) {
+		let node = new Node(value);
+
+		if (this.head == null){
+			this.head = node;
+		}else{
+			let current = this.head;
+			let i = 0;
+			while(i < this.size){
+				if (current.next == null){
+					current.next = node;
+				}else{
+					current = current.next;
+				}
+				
+				i++;
+			}
+		}
+		this.size += 1;
 	}
 
-	prepend(value) {
+	prepend(value){
+		let node = new Node(value);
+		
+		if (this.head == null){
+			this.head = node;
+		}else{
+			let current = node;
+			current.next = this.head;
+			this.head = current;
+
+			
+		}
+		this.size += 1;
 	}
 }
 
